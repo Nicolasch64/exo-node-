ba13b7e24c90b5a3eb89ddf3457b7d256a226c5e
Nicolasch64 <nidichapelle2000@yahoo.fr>
2025-01-10 17:03:37 +0100

    node commit

--- storage/todo.js
+++ storage/todo.js
@@ -1,5 +1,8 @@
+// exo to do list
+
 import { createServer } from "node:http";
 
 createServer(async (req, res) => {
+	res.setHeader("Content-Type", "application/json");
 	res.end();
 }).listen(4000);
