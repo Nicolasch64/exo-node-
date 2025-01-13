19cbb38c9451b173f18c1cecbdf4e424b0069d8a
Nicolasch64 <nidichapelle2000@yahoo.fr>
2025-01-13 14:57:01 +0100

    petit exo perso pour recupere une bio d artiste dans mon dossier

--- errorgen/index.js
+++ errorgen/index.js
@@ -1,39 +1,56 @@
-import {
-	readFileSyncError,
-	asyncApiError,
-	throwErreurperso,
-} from "./errorGenerator.js";
+// import {
+// 	readFileSyncError,
+// 	asyncApiError,
+// 	throwErreurperso,
+// } from "./errorGenerator.js";
 
-async function main() {
-	try {
-		console.log("tentative de lecture du fichier ");
-		readFileSyncError();
-	} catch (error) {
-		console.error("Erreur capturée (synchrones) :", error.message);
-		console.error("Stack trace :", error.stack);
-	} finally {
-		console.log("Nettoyage après tentative de lecture du fichier.");
+// async function main() {
+// 	try {
+// 		console.log("tentative de lecture du fichier ");
+// 		readFileSyncError();
+// 	} catch (error) {
+// 		console.error("Erreur capturée (synchrones) :", error.message);
+// 		console.error("Stack trace :", error.stack);
+// 	} finally {
+// 		console.log("Nettoyage après tentative de lecture du fichier.");
+// 	}
+
+// 	try {
+// 		console.log("tentative d appel api asynchrone");
+// 		await asyncApiError();
+// 	} catch (error) {
+// 		console.error("Erreur capturée (asynchrone) :", error.message);
+// 		console.error("Stack trace :", error.stack);
+// 	} finally {
+// 		console.log("Nettoyage après tentative d'appel API.");
+// 	}
+
+// 	try {
+// 		console.log("lancement de ma connerie");
+// 		throwErreurperso();
+// 	} catch (error) {
+// 		console.error("j ai capturé ta connerie:", error.message);
+// 		console.error("Stack trace :", error.stack);
+// 	} finally {
+// 		console.log("Nettoyage après lancement de ta connerie.");
+// 	}
+// }
+
+// main();
+
+import { fileURLToPath } from "url";
+import { dirname, join } from "path"; // Assure-toi d'importer `join` depuis 'path'
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = join(__dirname, "artist.txt");
+
+fs.readFile(filePath, "utf8", (err, data) => {
+	if (err) {
+		console.error("Error reading file:", err);
+		return;
 	}
-
-	try {
-		console.log("tentative d appel api asynchrone");
-		await asyncApiError();
-	} catch (error) {
-		console.error("Erreur capturée (asynchrone) :", error.message);
-		console.error("Stack trace :", error.stack);
-	} finally {
-		console.log("Nettoyage après tentative d'appel API.");
-	}
-
-	try {
-		console.log("lancement de ma connerie");
-		throwErreurperso();
-	} catch (error) {
-		console.error("j ai capturé ta connerie:", error.message);
-		console.error("Stack trace :", error.stack);
-	} finally {
-		console.log("Nettoyage après lancement de ta connerie.");
-	}
-}
-
-main();
+	console.log("File content:", data);
+});
